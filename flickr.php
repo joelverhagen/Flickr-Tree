546ad8569cf1dfe6097f6a6d7bea92151a12507a
Joel Verhagen <joel.verhagen@gmail.com>
2010-11-06 19:04:32 -0400

    Initial commit.

---
+++ flickr.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'key.php';
+
+if ( isset($_GET["tag"]) ) {
+	header("Content-type: application/xml");
+	$url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&sort=relavancy&api_key=".$apiKey."&tags=".$_GET["tag"]."&per_page=20&extras=url_m,url_sq,url_o,tags";
+	$pictures = new SimpleXMLElement($url, NULL, TRUE);
+	echo "<photos>\n";
+	foreach ( $pictures->photos->photo as $current ) {
+		echo "\t<photo full='".( isset($current["url_o"]) ? $current["url_o"] : $current["url_m"] )."' thumb='".$current["url_sq"]."' tags='".$current["tags"]."' />\n";
+	}
+	echo "</photos>";
+} 
+
+?>
